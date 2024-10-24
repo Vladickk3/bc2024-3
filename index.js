729018a112255a3c1f552baacf9bd642815565a6
Vladickk3 <kapik327x@gmail.com>
2024-10-24 10:47:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-﻿const fs = require('fs');
+const fs = require('fs');
 const { program } = require('commander');
 
+
 program
-    .requiredOption('-i, --input <file>', 'input file path')
-    .option('-o, --output <file>', 'output file path')
-    .option('-d, --display', 'display output in console');
+    .requiredOption('-i, --input <file>', 'input file path')   
+    .option('-o, --output <file>', 'output file path')         
+    .option('-d, --display', 'display output in console');     
 
 program.parse(process.argv);
-
 const options = program.opts();
+
 
 if (!options.input) {
     console.error("Please, specify input file");
     process.exit(1);
 }
 
+
 if (!fs.existsSync(options.input)) {
     console.error("Cannot find input file");
     process.exit(1);
 }
 
+
 const data = JSON.parse(fs.readFileSync(options.input, 'utf-8'));
 
-// Логіка для варіанту 6: фільтруємо дані по ключу "parent"
+
 const filteredData = data.filter(item => item.parent === "BS3_BanksLiab");
 
 if (filteredData.length === 0) {
@@ -30,17 +33,22 @@ if (filteredData.length === 0) {
     process.exit(0);
 }
 
-// Формуємо результат у форматі "Назва показника: значення"
-const result = filteredData.map(item => `${item.indicatorNameEng}:${item.value}`).join('\n');
 
-// Якщо вказано параметр --output, записуємо результат у файл
+const result = filteredData
+    .map(item => `${item.txten || 'indicator not found'}:${item.value}`)
+    .join('\n');
+
+
+if (!options.output && !options.display) {
+    process.exit(0);
+}
+
+
 if (options.output) {
     fs.writeFileSync(options.output, result);
 }
 
-// Якщо вказано параметр --display, виводимо результат у консоль
+
 if (options.display) {
     console.log(result);
 }
-
-// Якщо не задано жодних параметрів виводу, нічого не робимо
